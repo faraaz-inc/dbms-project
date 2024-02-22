16ee187613ba7a9fd5869cb235d531c60e84e11c
faraaz <faraaz.husayn@gmail.com>
2024-02-22 14:33:07 +0530

    Initialiased the project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+import { Signup } from './pages/signup'
+import { SignIn } from './pages/signin'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
